0bebb9449fbe25c9c6626b54ade2d66fb26af881
a.stolov <a.stolov@avantize.com>
2026-08-20 19:20:50 +0300

    fix: remove problematic characters from CRM admin password

--- deploy.sh
+++ deploy.sh
@@ -388,8 +388,8 @@ generate_secrets() {
   upper=$(tr -dc 'A-Z' </dev/urandom | head -c 1)
   lower=$(tr -dc 'a-z' </dev/urandom | head -c 1)
   digit=$(tr -dc '0-9' </dev/urandom | head -c 1)
-  special=$(tr -dc '!@#%^*' </dev/urandom | head -c 1)
-  rest=$(tr -dc 'A-Za-z0-9!@#%^*' </dev/urandom | head -c 12)
+  special=$(tr -dc '@#%^' </dev/urandom | head -c 1)
+  rest=$(tr -dc 'A-Za-z0-9@#%^' </dev/urandom | head -c 12)
 
   SEED_ADMIN_PASSWORD=$(echo "$upper$lower$digit$special$rest" | fold -w1 | shuf | tr -d '\n')
 
